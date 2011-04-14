2aa24cb409fcde1d362d4539fb1387e12a0febaa
Cyril Jose <cyril_jose@ymail.com>
2011-04-14 15:46:13 -0400

    Took out search file in dir method b/c its not really needed

--- test/tc_fastaParser.rb
+++ test/tc_fastaParser.rb
@@ -4,16 +4,19 @@ require 'testParse.rb'
 class TestArgs < Test::Unit::TestCase
   
   def test_args_and_ext
-    assert_equal(true, TestParser.new(ARGV[0] = 'ccgga', ARGV[1] ='test.fasta', ARGV[2] = 'results.txt').check_args_and_file_ext, "Doesn't work")
+    tp = TestParser.new('ccgga', 'test.fasta', 'results.txt')
+    assert(tp.check_args_and_file_ext == true)
   end
 
-  def test_fasta_exists
-    assert_equal(true, TestParser.new(ARGV[0] = 'ccgga', ARGV[1] ='test.fasta', ARGV[2] = 'results.txt' ).fasta_in_dir(ARGV[1] ='test.fasta'), "Doesn't work")
-  end
-=begin
+#  def test_fasta_exists
+#    tp = TestParser.new('ccgga', 'test.fasta', 'results.txt')
+#    assert(tp.fasta_in_dir == true) 
+#  end
+
   def test_if_fasta
-    assert_equal(true, TestParser.new(ARGV[0] = 'ccgga', ARGV[1] ='test.fasta', ARGV[2] = 'results.txt' ).check_first_symbol_in_fasta(ARGV[1] ='test.fasta'), "Doesn't work")         
+    tp = TestParser.new('ccgga', 'test.fasta', 'results.txt')
+    assert(tp.check_first_symbol_in_fasta == true)         
   end
-=end
+
 end 
 
--- test/testParse.rb
+++ test/testParse.rb
@@ -9,40 +9,40 @@ class TestParser
   end
   
   def check_args_and_file_ext 
-=begin
-   unless ARGV.length == 3 
+
+   unless self.query != nil && self.input_file != nil && self.output_file != nil 
       puts "Error => exception: <query> <your_fasta_file.fasta> <your_output.txt>"
-     exit
+     return false
     end
-    unless input_file.downcase[-6..-1] == '.fasta'
-      puts "Error => exception: Fasta file must be in '.fasta' format."
-      exit
-    end
-    puts "Args and file ext correct"
-=end
-    ARGV.length == 3 && input_file.downcase[-6..-1] == '.fasta' && output_file.downcase[-4..-1] == '.txt'
+    
+    self.input_file.downcase[-6..-1] == '.fasta'
+    return true
   end
   
-  def fasta_in_dir input_file
-    #if Dir[input_file] != []
-      #puts "File exists"
-      input_file == 'test.fasta'
-    #else
-      #puts "Error => exception: File not found"
-      #exit
-    #end  
+#  def fasta_in_dir
+#    if Dir[input_file] != []
+#      puts "File exists"
+#      return true
+#      #@input_file == 'test.fasta'
+#    else
+#      puts "Error => exception: File not found"
+#      return false
+#    end  
+#  end
+
+  def check_first_symbol_in_fasta
+    begin
+      open_fasta = File.open( @input_file, "r" )
+      seq_array = open_fasta.each_char.to_a
+      seq_array[0] == ">"
+      return true
+    rescue
+      return false
+    end
   end
-=begin  
-  def check_first_symbol_in_fasta input_file
-    open_fasta = File.open( input_file, "r" )
-    puts open_fasta
-    seq_array = open_fasta.each_char.to_a
-    puts seq_array[0] == ">"
-  end
-=end
+
 end
 
 test = TestParser.new( query = ARGV[0], input_file = ARGV[1], output_file = ARGV[2] )
 test.check_args_and_file_ext
-test.fasta_in_dir( input_file = ARGV[1] )
-#test.check_first_symbol_in_fasta( input_file = ARGV[1] )
+test.check_first_symbol_in_fasta
